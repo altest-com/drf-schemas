37e406403ae56a68ea2b39c30d1cc4ffdbda0990
raikel <raikelbl@gmail.com>
2020-04-21 08:10:25 -0500

    Removed build files added previously by error

--- build/lib/drf_schemas/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'drf_schemas.apps.DrfSchemasConfig'
--- build/lib/drf_schemas/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class DrfSchemasConfig(AppConfig):
-    name = 'drf_schemas'
-
-    def ready(self):
-        from . import signals
